f42d85052982057f28988e4f0b2ccf24fbe6a9d3
AntimaDwivedi <antimadwivedi28@gmail.com>
2024-01-21 20:01:59 +0530

    JPA and Hibernate

---
+++ JpaAndHibernate/src/test/java/com/springboot/jpahibernate/BasicsOfJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsOfJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
